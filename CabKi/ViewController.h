78419eab0a8e446ccc98549bcc28fb49eed81eed
Administrator <admin@Administrators-Mac.local>
2014-11-21 01:56:06 -0800

    Initial Commit

---
+++ CabKi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CabKi
+//
+//  Created by Administrator on 21/11/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CabKi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CabKi
+//
+//  Created by Administrator on 21/11/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
